790a6f7991b1cc81017bd97804cc6c39e7399427
Eriiiiiiiiiic <91080225+Eriiiiiiiiiic@users.noreply.github.com>
2022-06-12 20:24:19 +0200

    Pointer Fehler behoben

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     brett.display();
 
     std::cout << "Spieler 1 ist am Zug!\n";
-    brett.place_word("world",2,4,"d",*p1);
+    brett.place_word("world",2,4,"d",&p1);
     std::cout << "Spieler 1 hat gespielt!\n";
     std::cout << "Er hat "<< p1.get_score() <<" Punkte!\n";
 
@@ -25,15 +25,15 @@ int main() {
 
 
     std::cout << "Spieler 2 ist am Zug!\n";
-    brett.place_word("maths",5,2,"r",*p2);
+    brett.place_word("maths",5,2,"r",&p2);
     std::cout << "Spieler 2 hat gespielt!\n";
     std::cout << "Er hat "<< p2.get_score() <<" Punkte!\n";
 
     brett.display();
 
     std::cout << "Spieler 1 ist am Zug!\n";
-    brett.place_word("message",5,1,"d",*p1);
-    std::cout << "Fehler! Daher hat sich nichts verändert\n";
+    brett.place_word("message",5,1,"d",&p1);
+    std::cout << "Fehler! Daher hat sich nichts verÃ¤ndert\n";
     std::cout << "Er hat immernoch "<< p1.get_score() <<" Punkte!\n";
 
     brett.display();
