90ddef5dfffa491e28086ee6ac3c170b63a20ef1
Eriiiiiiiiiic <91080225+Eriiiiiiiiiic@users.noreply.github.com>
2022-06-13 09:30:48 +0200

    stein_setzen

--- Player.cpp
+++ Player.cpp
@@ -8,7 +8,7 @@ Player::Player(){
 void Player::steine_ziehen(Board* board){
     for(int i=0; i <= 6; i++){
         if(stein[i] != ' '){
-            int r = rand() % board->ziehbare_steine.length();      // Returns a pseudo-random integer between 0 and RAND_MAX.
+            int r = rand() % board->ziehbare_steine.length();      /* Zufällige Zahl von 0 bis zur Anzahl an Steinen -1*/
             stein[i] = char(board->ziehbare_steine[r]);
             board->ziehbare_steine.erase(r,r+1);
             /* Hier muss ein neuer Stein aus der Menge der Ziehbaren Steine gezogen weerden.
@@ -16,6 +16,17 @@ void Player::steine_ziehen(Board* board){
         }
     }
 }
+
+
+void Player::stein_setzen(char letter){
+    for(int i=0; i <= 6; i++){
+        if(stein[i] == letter){
+            stein[i] = ' ';
+            break;
+        }
+    }
+}
+
 
 void Player::add_to_score(int n){
     score += n;
