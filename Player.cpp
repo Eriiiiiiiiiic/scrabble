343af8fb7a0b2a918b0f0b0bf2ce60378e2f8121
Eriiiiiiiiiic <91080225+Eriiiiiiiiiic@users.noreply.github.com>
2022-06-14 21:53:31 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -5,15 +5,22 @@
 // ____________________________________________________________________________
 Player::Player() {
     score = 0;
+    stein[0] = ' ';
+    stein[1] = ' ';
+    stein[2] = ' ';
+    stein[3] = ' ';
+    stein[4] = ' ';
+    stein[5] = ' ';
+    stein[6] = ' ';
 }
 
 // ____________________________________________________________________________
 void Player::steine_ziehen(Board* board) {
     for (int i = 0; i <= 6; i++) {
-        if(stein[i] != ' ') {
-            int r = rand() % board->ziehbare_steine.length();      /* Zufällige Zahl von 0 bis zur Anzahl an Steinen -1*/
+        if(stein[i] == ' ' && board->ziehbare_steine.length()!= 0) {  /*&& board->ziehbare_steine.length()!= 0  WICHTIG FEHLT */
+            int r = rand() % board->ziehbare_steine.length();      /* ZufÃ¤llige Zahl von 0 bis zur Anzahl an Steinen -1*/
             stein[i] = char(board->ziehbare_steine[r]);
-            board->ziehbare_steine.erase(r,r+1);
+            board->ziehbare_steine.erase(r,1);
             /* Hier muss ein neuer Stein aus der Menge der Ziehbaren Steine gezogen weerden.
                Aus dieser Menge muss der Stein entfertnt werden. */
         }
@@ -39,3 +46,14 @@ void Player::add_to_score(int n) {
 int Player::get_score() {
     return score;
 }
+
+// ____________________________________________________________________________
+char* Player::get_steine() {
+    return stein;
+}
+
+
+void Player::display(){
+    std::cout <<"\n ~~~~~~=" << stein[0] << stein[1] << stein[2] << stein[3] << stein[4] << stein[5] << stein[6] << "=~~~~~~ \n\n" ;
+}
+
