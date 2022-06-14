5fc7f11c4e2c0ea0584d03789b14e81aef9e8335
Eriiiiiiiiiic <91080225+Eriiiiiiiiiic@users.noreply.github.com>
2022-06-14 21:52:36 +0200

    Spielbare loop
    Es fehlen Endbedingungen, Umlaute, Das Prüfen vom Wort, Das ob man die nötigen Buchstaben hat, die Score Funktion ......

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "./Player.h"
 #include "./setcolor.h"
 
+using namespace std;
+
 int main() {
 
     srand(time(NULL));  // Random-Seed initalisieren
@@ -15,30 +17,66 @@ int main() {
     Player p1;
     Player p2;
 
+    (&p1)->steine_ziehen(&brett);
+    (&p2)->steine_ziehen(&brett);
+
     setcolor( 0x07 );  // Initial color for program
 
-    brett.display();
+    for(int i=0; i <= 10; i++){  /* Das abbruchskriterium ist noch nicht ganz richtig */
+        int x = 0;
+        int y = 0;
+        string word = "";
+        string direction = "";
 
-    std::cout << "Spieler 1 ist am Zug!\n";
-    brett.place_word("worlds",2,4,"d",&p1);
-    std::cout << "Spieler 1 hat gespielt!\n";
-    std::cout << "Er hat " << p1.get_score() << " Punkte!\n";
+        if(i%2 == 0){
+            cout << "Spieler 1 ist am Zug! Hier das aktuelle Brett:\n";
+            brett.display();
+            p1.display();
 
-    brett.display();
+            cout << "Wo soll dein Wort beginnen? (x Koordinate):\n";
+            cin >> x;
+            cout << "Wo soll dein Wort beginnen? (y Koordinate):\n";
+            cin >> y;
 
-    std::cout << "Spieler 2 ist am Zug!\n";
-    brett.place_word("Maths",5,2,"r",&p2);
-    std::cout << "Spieler 2 hat gespielt!\n";
-    std::cout << "Er hat " << p2.get_score() << " Punkte!\n";
+            cout << "Was ist dein Wort?:\n";
+            cin >> word;
 
-    brett.display();
+            cout << "Vertikal oder Horizontal?(v,h):\n";
+            cin >> direction;
 
-    std::cout << "Spieler 1 ist am Zug!\n";
-    brett.place_word("message",5,1,"d",&p1);
-    std::cout << "Fehler! Daher hat sich nichts verändert\n";
-    std::cout << "Er hat immernoch " << p1.get_score() << " Punkte!\n";
+            brett.place_word(word,x,y,direction,&p1);
+            cout << "Spieler 1 hat gespielt!\n\n";
+            cout << "Er hat " << p1.get_score() << " Punkte!\n\n";
+            cout << "Er zieht neue Steine:\n";
+            p1.display();
+            cout << "\n\n\n";
+        }
+        else{
+            cout << "Spieler 2 ist am Zug! Hier das aktuelle Brett:\n";
+            brett.display();
+            p2.display();
 
-    brett.display();
+            cout << "Wo soll dein Wort beginnen? (x Koordinate):\n";
+            cin >> x;
+            cout << "Wo soll dein Wort beginnen? (y Koordinate):\n";
+            cin >> y;
+
+            cout << "Was ist dein Wort?:\n";
+            cin >> word;
+
+            cout << "Vertikal oder Horizontal?(v,h):\n";
+            cin >> direction;
+
+
+            brett.place_word(word,x,y,direction,&p2);
+            cout << "Spieler 2 hat gespielt!\n\n";
+            cout << "Er hat " << p2.get_score() << " Punkte!\n\n";
+            cout << "Er zieht neue Steine:\n";
+            p2.display();
+            cout << "\n\n\n";
+        }
+    }
 
     return 0;
 }
+
