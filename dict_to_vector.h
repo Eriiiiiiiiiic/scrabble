2790a2b2c0c3199eb63407055eefaa47bf0c9ac3
Eriiiiiiiiiic <91080225+Eriiiiiiiiiic@users.noreply.github.com>
2022-07-05 13:59:04 +0200

    fast search works!

---
+++ dict_to_vector.h
@@ -0,0 +1,21 @@
+#ifndef DICT_TO_VECTOR_H
+#define DICT_TO_VECTOR_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+vector<string>  dict_vector() { // file muss 2^k Zeilen haben!
+    vector<string> dict;
+    string file_word; // Ein Speicherplatz für die Wörter im dicitionary
+    ifstream fin;
+    fin.open("dictionary.txt",ios::in); //öffnet die Datei zum lesen
+    while(!fin.eof()){ //Solange man nicht am end-of-file ankommt oder es noch nicht gefunden hat, mache weiter.
+        fin>>file_word;       //Neues Wort aus der Datei erhalten.
+        dict.push_back(file_word); //in Diesem Fall ist das Wort nicht existent, also meldet es einen Fehler.
+    }
+    return dict;
+}
+#endif  // DICT_TO_VECTOR_H
