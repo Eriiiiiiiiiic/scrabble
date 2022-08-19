33be25e1c5ebccedfba560c6c2c9af610238f5f3
Eriiiiiiiiiic <91080225+Eriiiiiiiiiic@users.noreply.github.com>
2022-08-19 14:30:12 +0200

    parameter

--- main.cpp
+++ main.cpp
@@ -1,292 +1,253 @@
-//Projekt von Paul Schuh und Eric Trébuchon
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <time.h>
-#include <stdlib.h>
-#include "./Board.h"
-#include "./Player.h"
-
-using namespace std;
+function main
 
 
-int consecutive_passed_turns = 0;  // Globale Variable zum Zaehlen gepasster Zuege.
-bool standard_ASCII = false;
+% COBWEB
+netz = 4;
+int_method = 1;
+energy_plot = 1;
+
+%Spinnennetz
+if netz == 1
+%%{
+  nc = 5;
+  c = 3;
+
+  fixpoints = [nc*c+2 - nc: nc*c+1];
+  d = 0.2;
+  k = 1;  %Federkonstante
+  m = 1;  %Masse
+  g = 0.01;  #Gravitation
+  friction = 1;  %Reibungskoeffizient
+  verb1 = connections(nc,c);
+  p_0 = [0,0,0.5,-0.5];
+  C1 = matrix(nc,c,p_0);
+  C2 = realistic_cobweb(d,verb1,C1){1};
+  C3(1:2,:) = C2;
+  P = [transpose(p_0(3:4)),zeros(2,length(C2)-1)];
+  C3(3:4,:) = P;
+  verb2 = realistic_cobweb(d,verb1,C1){2};  
+  verb = imp_verb(verb2);
+  DistMat = dist_neu(length(C3),C3,verb);
+  length(DistMat);
+  n = length(C3);
+  s = 1;
+  C = stretch(C3,s);
+%}
+endif
+
+% HÄNGENDE KETTE
+if netz == 2
+  %%{
+  n = 30;
+  fixpoints = [1,n];
+  k = 1;  %Federkonstante
+  m = 3;  %Masse
+  g = 1;  #Gravitation
+  friction = 0;  %Reibungskoeffizient
+  verb = {{2}};
+  for i = 2:(n-1)
+    verb{i}{1} = i-1;
+    verb{i}{2} = i+1;
+  end
+  verb{n}{1} = n-1;
+  C = [(1/n)* 1:n; 1:n; zeros(1,n); zeros(1,n)];
+  DistMat = dist_neu(n,C,verb);
+  %}
+endif
+% LIANE
+if netz == 3
+%%{
+  n = 20;
+  fixpoints = [1];
+  k = 10;  %Federkonstante
+  m = 1;  %Masse
+  g = 10;  #Gravitation
+  friction = 0;  %Reibungskoeffizient
+  verb = {{2}};
+  for i = 2:(n-1)
+    verb{i}{1} = i-1;
+    verb{i}{2} = i+1;
+  end
+  verb{n}{1} = n-1;
+  C = [(1/n)* 1:n; 1:n; zeros(1,n); zeros(1,n)];
+  DistMat = dist_neu(n,C,verb);
+%}
+endif
+% Gitter
+if netz == 4
+%%{
+  h = 5;
+  w = 4;
+  d = 0.1
+  n = h*w;
+  fixpoints = [(n-w+1):n];
+  k = 2;  %Federkonstante
+  m = 1;  %Masse
+  g = 0.05;  #Gravitation
+  friction = 0;  %Reibungskoeffizient
+  C1 = [repmat(1:w,1,h); floor(((1:n) - 1)/w); zeros(1,n); 0.2 zeros(1,n-1)];
+  C1(3,n/2) = 2;
+  C1(4,n/2) = -2;
+  verb1 = gitter(w,h);
+  C2 = realistic_cobweb(d,verb1,C1){1};
+  C3(1:2,:) = C2;
+  P = zeros(2,length(C2));
+  C3(3:4,:) = P;
+  verb2 = realistic_cobweb(d,verb1,C1){2};  
+  verb = imp_verb(verb2);
+  DistMat = dist_neu(length(C3),C3,verb);
+  length(DistMat);
+  n = length(C3);
+  s = 1;
+  C = stretch(C3,s);
+%}
+endif
 
 
-bool game_loop(Board *brett, Player *p, bool is_player1) {
-    markierung:
-    int x = 0;
-    int y = 0;
-    string coords = "";
-    string word = "";
-    string direction = "";
-    int start_score = p->get_score();
+  
+  
+  
+  gamma = 0.5;
+  beta = 0.25;
+  
+  tau = 1/8;
+  T = 100;
+  framerate = 4 % jedes ***-te Bild wird gezeichnet
+  timesteps = floor(T/tau)
 
-    if (is_player1) cout << p->name << " ist am Zug! Hier das aktuelle Brett:\n";
-    else cout << p->name  << " ist am Zug! Hier das aktuelle Brett:\n";
-    brett->display();
-    p->display(standard_ASCII);
-
-    cout << "Wo soll dein Wort beginnen?: ";
-    cin >> coords;
-    // Wird statt Koordinaten ein "*" eingegeben, so koennen Steine aus der Hand
-    // mit Steinen aus dem Beutel getauscht werden.
-    if (coords == "*") {
-        if (brett->ziehbare_steine.length() < 7) {
-            cout << "Es sind nur noch weniger als sieben Steine im Beutel. ";
-            cout << "Daher koennen keine Steine mehr getauscht werden!\n";
-            string skip = "";
-            cout << "Moechtest du eine Runde aussetzen? (y/n): ";
-            cin >> skip;
-            if (skip == "y") {  // Zug aussetzen
-                if (is_player1) cout << p->name  << " setzt eine Runde aus.\n\n\n\n";
-                else cout << p->name  << " setzt eine Runde aus.\n\n\n\n";
-                consecutive_passed_turns++;
-                return true;
-            }
-            goto markierung;
-        }
-        cout << "Welche Steine moechtest du tauschen?: ";
-        cin >> word;
-        if (word == "*") {  // Zug aussetzen
-            if (is_player1) cout << p->name  << " setzt eine Runde aus.\n\n\n\n";
-            else cout << p->name  << " setzt eine Runde aus.\n\n\n\n";
-            consecutive_passed_turns++;
-            return true;
-        }
-        if (p->steine_tauschen(brett, word)) {
-            if (is_player1) cout << p->name  << " hat Steine weggelegt.\n";
-            else cout << p->name  << " hat Steine weggelegt.\n";
-            cout << "Er zieht neue Steine:\n";
-            p->display(standard_ASCII);
-            cout << "\n\n\n";
-            consecutive_passed_turns = 0;
-            return true;
-        } else {
-            cout << "Du besitzt nicht die Steine, die du tauschen moechtest.\n";
-            cout << "Die Angabe der zu tauschenden Steine sollte z.B. AX&, oder ";
-            cout << "XXAC sein, falls du etwa die Steine ABCLXX& besitzt.\n";
-            cout << "Mit der Eingabe 'Welche Steine moechtest du tauschen?: *' ";
-            cout << "kannst du eine Runde aussetzen.\n\n";
-            goto markierung;
-        }
-    }
-    // coords in int Koordinaten umwandeln
-    // Die Koordinaten werden in Zahlen zwischen 0 und 14 umgewandelt.
-    x = int (coords[0]) - 65;
-    if (x >= 32) x -= 32;
-    try {
-        y = stoi(coords.substr(1)) - 1;
-    } catch (const invalid_argument& ex) {
-        cout << "\nBitte gib die Koordinaten in der Form xn an, dabei ist:\n";
-        cout << " - x ein Buchstabe zwischen a und o (x Koordinate)\n";
-        cout << " - n eine ganze Zahl zwischen 1 und 15 (y Koordinate)\n";
-        cout << "Beispiele: b7, f15, a3, o11\n";
-        cout << "Mit der Eingabe 'Wo soll dein Wort beginnen?: *' ";
-        cout << "kannst du Steine von der Hand mit Steinen aus dem ";
-        cout << "Beutel tauschen, oder eine Runde aussetzen.\n\n";
-        goto markierung;
-    }
-    if (x < 0 || x > 14 || y < 0 || y > 14) {
-        cout << "\nBitte gib die Koordinaten in der Form xn an, dabei ist:\n";
-        cout << " - x ein Buchstabe zwischen a und o (x Koordinate)\n";
-        cout << " - n eine ganze Zahl zwischen 1 und 15 (y Koordinate)\n";
-        cout << "Beispiele: b7, f15, a3, o11\n";
-        cout << "Mit der Eingabe 'Wo soll dein Wort beginnen?: *' ";
-        cout << "kannst du Steine von der Hand mit Steinen aus dem ";
-        cout << "Beutel tauschen, oder eine Runde aussetzen.\n\n";
-        goto markierung;
-    }
+  
+  X_initial = transpose(transpose(C)(:));
+  X = X_initial; % Zeilenmatrix
+  
+  positions_stored = zeros(2*n,timesteps);
 
 
-    string word_j = "";
-    cout << "Was ist dein Wort?: ";
-    cin >> word_j;
+  figure(2)
+  clf;
+  for t = 1:timesteps
+    t
+    if int_method == 1
+      
+    %Verlet Method
+    %%{
+    f_temp = f(X,n,fixpoints,verb,tau,k,m,friction,g,DistMat);
+    temp_velocity = X(2*n+1:4*n) + tau/2 * f_temp;
+    X(1:2*n) = X(1:2*n) + tau * temp_velocity;
+    X(2*n+1:4*n) = X(2*n+1:4*n) + tau/2 * f(X,n,fixpoints,verb,tau,k,m,friction,g,DistMat) +  tau/2 * f_temp;
+    %}
+    %initale Werte für Fixpunktiteration
+    
+    endif
+    if int_method == 2
+    %%{
 
-    int index1 = 0;
-    int index2 = 0;
-    bool joker_pos[15] = {};
-    while (index1 < word_j.length()) {
-        if (word_j[index1] == '&') {
-            index1++;
-            char ch = word_j[index1];
-            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-                word.push_back(ch);
-            } else {
-                cout << "Das eingegebene Wort ist ungueltig!\n\n";  // Hier koennte noch eine genauere Fehlermeldung ausgegeben werden.
-                goto markierung;
-            }
-            joker_pos[index2] = true;
-            index2++;
-        } else {
-            char ch = word_j[index1];
-            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-                word.push_back(ch);
-            } else {
-                cout << "Das eingegebene Wort ist ungueltig!\n\n";  // Hier koennte noch eine genauere Fehlermeldung ausgegeben werden.
-                goto markierung;
-            }
-            index2++;
-            if (index2 >= 15) {
-                cout << "Das eingegebene Wort ist zu lang!\n\n";
-                goto markierung;
-            }
-        }
-        index1++;
-    }
+    x_old = X(1,1:(2*n));
+    v_old = X(1,(2*n+1):(4*n));
+    a_old = 1/m * f(X,n,fixpoints,verb,tau,k,m,friction,g,DistMat);
+
+    x_new = x_old;
+    v_new = v_old;
+    a_new = a_old;
 
 
-    cout << "Vertikal oder Horizontal?(v,h): ";
-    cin >> direction;
-    if (!(direction == "v" || direction == "h")) {
-        cout << "Bitte gib eine gueltige Richtung an.\n\n";
-        goto markierung;
-    }
-    cout << "\n";
 
-    int anzahl_steine_inital = p->get_num_steine(); //Um Bingos zu erkennen
+    %Fixpunktiteration
+    iter = 0;
 
-    if (brett->move_is_valid(word, x, y, direction)) {
-        if (p->wort_setzen(brett, word, joker_pos, x, y, direction)) {
-            brett->place_word(word, x, y, direction, p); //hier koennen bei schlechtem Input Fehler gethrowed werden und dann gecatched.
+    do
+      v_new_temp = v_old + (1-gamma)*tau*a_old + gamma*tau*a_new;
+      x_new_temp = x_old + tau* v_new + tau**2/2*((1-2*beta)*a_old + 2*beta*a_new);
+      a_new_temp = 1/m * f([x_new,v_new],n,fixpoints,verb,tau,k,m,friction,g,DistMat); %X durch X_new ersetzten
 
-            if((p->get_num_steine()) - anzahl_steine_inital == -7){
-                cout << "\nBINGO! Glückwunsch, du hast alle Steine gleichzeitig platziert, das sind 50 Bonuspunkte!\n\n";
-                p->add_to_score(50);
-            }
+      error = norm(x_new - x_new_temp);
 
-        }
-        else {
-            cout << "Dir fehlen die noetigen Steine!\n";
-            cout << "Versuche ein anderes Wort.\n\n";
-            goto markierung;
-        }
-    }
-    else {
-        cout << "Dein Wort ist fehlerhaft!\n";
-        cout << "Versuche ein anderes Wort.\n\n";
-        goto markierung;
-    }
+      v_new = v_new_temp;
+      x_new = x_new_temp;
+      a_new = a_new_temp;
 
-    if (is_player1) cout << p->name  << " hat gespielt!\n\n";
-    else cout << p->name  << " hat gespielt!\n\n";
-    cout << "Er hat " << (p->get_score()-start_score) << " Punkte bekommen!\n";
-    cout << "Gesamtpunktzahl: " << p->get_score()<< "\n\n";
-    if (brett->ziehbare_steine.length() == 0 && p->get_num_steine() == 0) { // Siegbedingung pruefen
-        if (is_player1) cout << p->name  << " hat keine Steine und kann keine Steine mehr ziehen. ";
-        else cout << p->name  << " hat keine Steine und kann keine Steine mehr ziehen. ";
-        cout << "Damit ist das Spiel beendet!\n";
-        cout << endl << "Hier ist das finale Brett:\n";
-        brett->display();
-        consecutive_passed_turns = 0;
-        return false;
-    }
-    cout << "Er zieht neue Steine:\n";
-    p->display(standard_ASCII);
-    cout << "\n\n\n";
 
-    brett->first_word_placed = true;  // Das erste Wort wurde schon gesetzt.
+      iter += 1;
+    until(iter > 50 || error < 10**(-3))
 
-    consecutive_passed_turns = 0;
-    return true;
-}
 
-int main() {
-    cout <<"\n"
-    "\n"
-    "   ______________________________________________________________ \n"
-    " / \\                                                             \\\n"
-    "|   |                                                            |\n"
-    " \\_ |                              _     _     _                 |\n"
-    "    |                             | |   | |   | |                |\n"
-    "    |           ___  ___ _ __ __ _| |__ | |__ | | ___            |\n"
-    "    |          / __|/ __| '__/ _` | '_ \\| '_ \\| |/ _ \\           |\n"
-    "    |          \\__ \\ (__| | | (_| | |_) | |_) | |  __/           |\n"
-    "    |          |___/\\___|_|  \\__,_|_.__/|_.__/|_|\\___|           |\n"
-    "    |                                                            |\n"
-    "    |                                                            |\n"
-    "    |    ________________________________________________________|___\n"
-    "    |  /                                                            /\n"
-    "    \\_/____________________________________________________________/\n\n\n";
+    %X aus x_new,v_new berechnen
+    X = [x_new,v_new];
+    endif
+
+    %}
+
+
+    positions_stored(:,t) = X(1:2*n);
 
 
 
 
+    if energy_plot == 1
+      %%{
+      if t!=1
+        energy_temp = energy;
+        kin_energy_temp = kin_energy;
+        spannungs_energy_temp  = spannungs_energy;
+        potentielle_energy_temp = potentielle_energy;
+      end
+      energy = 0;
+      kin_energy = 0;
+      spannungs_energy = 0;
+      potentielle_energy = 0;
+      for i = 1:n
+        kin_energy += 1/2 * m * 1/tau * norm([X(1,2*n+i),X(1,3*n+i)])^2; #kinetische Energie
+        for l = 1 : length(verb{i}) 
+          j = DistMat{i}{l}(1);% alle Verbindungen zu i
+          d = DistMat{i}{l}(2);
+          spannungs_energy += 1/4 * 1/tau * k * (norm([X(1,i)-X(1,j),X(1,n+i)-X(1,n+j)]) - d)^2; #spann/potentielle Ernergie. Der Fakotr 1/4 kommt davon, dass die Federn doppelt gezählt werden.
+        end
+        potentielle_energy += m*g*X(1,n+i);
+      end
+      energy = kin_energy + spannungs_energy + potentielle_energy;
+      if t!=1
+        %axis
+        xlabel ("rot: totale Energie,  blau: kin,  grün: spann,  gelb: pot"); 
+        plot([t/timesteps,(t+1)/timesteps],[energy_temp,energy],'r-'); hold on;
+        plot([t/timesteps,(t+1)/timesteps],[kin_energy_temp,kin_energy],'bo'); hold on;
+        plot([t/timesteps,(t+1)/timesteps],[spannungs_energy_temp,spannungs_energy],'g--'); hold on;
+        plot([t/timesteps,(t+1)/timesteps],[potentielle_energy_temp,potentielle_energy],'yx'); hold on;
+        energy; #output im Befehlsfenster
+      end
+      %}
+    endif
+  end
 
-    srand(time(NULL));  // Random-Seed initalisieren
 
-    Board brett;
-    Player p1;
-    Player p2;
+  figHandle = figure(1);
+  figure(1);
 
-    p1.steine_ziehen(&brett);
-    p2.steine_ziehen(&brett);
+  %axis([-1,9,-25,10]);
+  %axis([-3,3*w+1,-h,3*h+1]);
+  axis([-1,5,-8,5]);
+  clf;
 
-    string name1;
-    string name2;
-    cout << "Herzlich Willkommen zu Scrabble!\n\n";
-    cout << "Spieler 1, geben Sie Ihren Namen ein:\n";
-    cin >> name1;
-    p1.set_name_to(name1);
+  
+  for t = 1:timesteps
+    t
+    if rem(t,framerate) == 0
+      clf;
+      x = positions_stored(1:n,t); % die x Werte der n Punkte
+      y = positions_stored(n+1:2*n,t);  % die y Werte der n Punkte
 
-    cout << "\n";
-    cout << "Spieler 2, geben Sie Ihren Namen ein:\n";
-    cin >> name2;
-    p2.set_name_to(name2);
-    cout << "\n\n";
+        % Zeichnet die Punkte.
+      %axis([-10,10,-10,10]);
+      %axis([-3,2*w+1,-h,2*h+1]);
+      axis([-1,5,-8,5]);
 
-    int runde = 0;
-    while (true) {
-        // Einen Spielzug durchfuehren
-        if (runde%2 == 0) {
-            // Spieler 1 ist am Zug.
-            if (!game_loop(&brett, &p1, true)) {
-                // Spieler 1 hat das Spiel beendet.
-                cout << name1 << " hat " << p1.get_score() << " Punkte und erhaelt zusaetzliche Punkte:\n";
-                brett.final_letters_score(p2.get_steine_lst(), &p1, true);
-                cout << "Insgesamt hat er also " << p1.get_score() << " Punkte.\n\n";
-                cout << name2 << " hat " << p2.get_score() << " Punkte, doch es werden die Werte der uebrigen Steine abgezogen:\n";
-                brett.final_letters_score(p2.get_steine_lst(), &p2, false);
-                cout << "Insgesamt hat er also " << p2.get_score() << " Punkte.\n\n";
-                break;
-            }
-        } else {
-            // Spieler 2 ist am Zug.
-            if (!game_loop(&brett, &p2, false)) {
-                // Spieler 2 hat das Spiel beendet.
-                cout << name1 << " hat " << p1.get_score() << " Punkte, doch es werden die Werte der uebrigen Steine abgezogen:\n";
-                brett.final_letters_score(p1.get_steine_lst(), &p1, false);
-                cout << "Insgesamt hat er also noch " << p1.get_score() << " Punkte.\n\n";
-                cout << name2 << " hat " << p2.get_score() << " Punkte und erhaelt zusaetzliche Punkte:\n";
-                brett.final_letters_score(p1.get_steine_lst(), &p2, true);
-                cout << "Insgesamt hat er also " << p2.get_score() << " Punkte.\n\n";
-                break;
-            }
-        }
-        if (consecutive_passed_turns >= 4) {
-            // Beide Spieler haben zweimal hintereinander gepasst, also ist das Spiel beendet.
-            cout << "Da beide Spieler in zwei aufeinanderfolgenden Runden gepasst haben, endet das Spiel.\n\n";
-            cout << name1 << " hat " << p1.get_score() << " Punkte, doch es werden die Werte der uebrigen Steine abgezogen:\n";
-            brett.final_letters_score(p1.get_steine_lst(), &p1, false);
-            cout << "Insgesamt hat er also noch " << p1.get_score() << " Punkte.\n\n";
-            cout << name2 << " hat " << p2.get_score() << " Punkte, doch es werden die Werte der uebrigen Steine abgezogen:\n";
-            brett.final_letters_score(p2.get_steine_lst(), &p2, false);
-            cout << "Insgesamt hat er also " << p2.get_score() << " Punkte.\n\n";
-            break;
-        }
-        runde++; // Rundenzaehler erhoehen
-    }
-    if (p1.get_score() > p2.get_score()) {
-        cout << "Damit gewinnt "<< name1 << ". Herzlichen Glueckwunsch!\n\n";
-        if (!standard_ASCII) cout << "♪┏(°.°)┛┗(°.°)┓┗(°.°)┛┏(°.°)┓ ♪ ♪┏(°.°)┛┗(°.°)┓┗(°.°)┛┏(°.°)┓ ♪ \n\n\n\n\n\n   ";
-    } else if (p1.get_score() == p2.get_score()) {
-        cout << "Es gibt also einen Gleichstand.";
-    } else {
-        cout << "Damit gewinnt "<< name2 << ". Herzlichen Glueckwunsch!\n\n";
-        if (!standard_ASCII) cout << "♪┏(°.°)┛┗(°.°)┓┗(°.°)┛┏(°.°)┓ ♪ ♪┏(°.°)┛┗(°.°)┓┗(°.°)┛┏(°.°)┓ ♪ \n\n\n\n\n\n   ";
-    }
+      for i = 1:n
+        for j = cell2mat(verb{i})
+          line([x(i),x(j)],[y(i),y(j)]);
+        end
+      end
+      drawnow;
+      MakeGif(figHandle, 'test.gif');
+    end
+  end
+end
 
-    return 0;
-}
+main() %führe alles aus
